bb42ef1f5ed9cff0f67e0a043f89b78f0730e2e6
atv57 <155875754+atv57@users.noreply.github.com>
2024-01-07 09:08:35 +0000

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
 function runaway(id) {
-  id.style.top = Math.round(Math.random() * 800) + 'px';
+  id.style.top = Math.round(Math.random() * 600) + 'px';
   id.style.left = Math.round(Math.random()*1500) + 'px';
 }
